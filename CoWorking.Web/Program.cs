82ac7315b923277847757ebee9ca4b64d70ef14c
mayor.sheka <mayor.sheka@ukr.net>
2022-09-11 00:27:06 +0300

    open angular

--- CoWorking.Web/Program.cs
+++ CoWorking.Web/Program.cs
@@ -63,10 +63,10 @@ app.UseEndpoints(endpoints =>
     endpoints.MapControllers();
 });
 
-//app.UseSpa(spa =>
-//{
-//    spa.Options.SourcePath = "ClientApp";
-//    spa.UseAngularCliServer(npmScript: "start");
-//});
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+    spa.UseAngularCliServer(npmScript: "start");
+});
 
 app.Run();
